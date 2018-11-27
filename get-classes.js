386d40195bce4261e3a1ecea1ac4c1273526af24
Jacob Johnson <john6805@stthomas.edu>
2018-11-27 15:06:59 -0600

    class times functional

--- get-classes.js
+++ get-classes.js
@@ -7,6 +7,15 @@ var $;
 var courses = [];
 var sections = [];
 var subjects = ['ACCT','ACSC','ACST','AERO','AMBA','ARAB','ARHS','ARTH','BCHM','BCOM','BETH','BIOL','BLAW','BUSN','CATH','CHDC','CHEM','CHIN','CIED','CISC','CJUS','CLAS','COAC','COJO','COMM','CPSY','CSIS','CSMA','CTED','DRSW','DSCI','DVDM','DVDT','DVHS','DVLS','DVMT','DVPH','DVPM','DVPT','DVSP','DVSS','DVST','ECMP','ECON','EDCE','EDLD','EDUA','EDUC','EGED','ENGL','ENGR','ENTR','ENVR','ESCI','ETLS','EXSC','FAST','FILM','FINC','FREN','GBEC','GENG','GEOG','GEOL','GERM','GIFT','GMUS','GRED','GREK','GRPE','GRSW','GSPA','HIST','HLTH','HONR','HRDO','IBUS','IDSC','IDSW','IDTH','INAC','INCH','INEC','INEG','INFC','INFR','INGR','INHR','INID','INIM','INJP','INLW','INMC','INMG','INMK','INOP','INPS','INRS','INSP','INST','INTR','IRGA','ITAL','JAPN','JOUR','JPST','LATN','LAWS','LEAD','LGST','LHDT','MATH','MBAC','MBEC','MBEN','MBEX','MBFC','MBFR','MBFS','MBGC','MBGM','MBHC','MBHR','MBIF','MBIM','MBIS','MBLW','MBMG','MBMK','MBNP','MBOP','MBQM','MBSK','MBSP','MBST','MBUN','MBVE','MFGS','MGMP','MGMT','MKTG','MMUS','MSQS','MSRA','MUSC','MUSN','MUSP','MUSR','MUSW','NSCI','ODOC','OPMT','PHED','PHIL','PHYS','PLLD','POLS','PSYC','PUBH','QMCS','READ','REAL','RECE','REDP','RUSS','SABC','SABD','SACS','SAED','SAIM','SAIN','SALS','SAMB','SASE','SASW','SEAM','SEIS','SMEE','SOCI','SOWK','SPAN','SPED','SPGT','SPUG','STAT','STEM','TEGR','THEO','THTR','WMST']
+var days = {
+    0: 'M',
+    1: 'T',
+    2: 'W',
+    3: 'Th',
+    4: 'F',
+    5: 'Sa',
+    6: 'Su'
+}
 // axios.get('https://classes.aws.stthomas.edu/json/getSubjectList.json?year=2019&term=20&schoolCode=ALL&levelCode=ALL')
 // .then(result => {
 //     console.log(result.data);
@@ -74,6 +83,32 @@ axios.get('https://classes.aws.stthomas.edu/index.htm?year=2019&term=20&schoolCo
         section.professors = (section.professors === '') ? 'TBD' : section.professors;
 
         var schedule = $(element).find('table[class="courseCalendar"]').find('tr');
+        schedule.each((function(j, row){
+            if(j == 2)
+            {
+                $(row).find($('td')).each((k, cell) => {
+                    if($(cell).hasClass('time'))
+                    {
+                        if(section.times.length != 0)
+                        {
+                            section.times = section.times + ',';
+                        }
+                        var timeRange = $(cell).text();
+                        var amIndex = timeRange.indexOf('am');
+                        var pmIndex = timeRange.indexOf('pm');
+                        if(amIndex != -1)
+                        {
+                            timeRange = timeRange.slice(0, amIndex + 2) + '-' + timeRange.slice(amIndex + 2);
+                        }
+                        else
+                        {
+                            timeRange = timeRange.slice(0, pmIndex + 2) + '-' + timeRange.slice(pmIndex + 2);
+                        }
+                        section.times = section.times + days[k] + ' ' + timeRange;
+                    }
+                })    
+            }
+        }));
         //console.log(schedule);
 
         sections.push(section);
@@ -82,10 +117,11 @@ axios.get('https://classes.aws.stthomas.edu/index.htm?year=2019&term=20&schoolCo
     //var courses = body.find('div[class=course]')[0];
     //var section = body.find('span[class=courseOpen]')[0];
 
-    for(var i = 0; i < sections.length; i++)
-    {
-        console.log(sections[i]);
-    }
+    console.log(sections[0]);
+    // for(var i = 0; i < sections.length; i++)
+    // {
+    //     console.log(sections[i]);
+    // }
 })
 .catch(error => {
     console.log(error);
