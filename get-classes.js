416e4927a9be46f747e3b056b8619330e301a38c
Jacob Johnson <john6805@stthomas.edu>
2018-11-28 12:09:50 -0600

    create database in file

--- get-classes.js
+++ get-classes.js
@@ -1,5 +1,7 @@
 var cheerio = require('cheerio');
 var axios = require('axios');
+var sqlite = require('sqlite3').verbose();
+var database = new sqlite.Database('ust_courses.sqlite3');
 var bodyStart;
 var bodyEnd;
 var body;
@@ -18,7 +20,14 @@ var days = {
     6: 'Su'
 }
 //Create database
-//Create Tables
+database.serialize(() => {
+    //Create Tables
+    database.run('CREATE TABLE departments(subject TEXT PRIMARY KEY, full_name TEXT);');
+    database.run('CREATE TABLE courses(subject TEXT, course_number TEXT, credits INT, name TEXT, description TEXT, PRIMARY KEY(subject, course_number));');
+    database.run('CREATE TABLE sections(crn INT PRIMARY KEY, subject TEXT, course_number TEXT, section_number TEXT, building TEXT, room TEXT, professors TEXT, times TEXT, capacity INT, registered TEXT);');
+    database.run('CREATE TABLE people(university_id INT PRIMARY KEY, position TEXT, password TEXT, first_name TEXT, last_name TEXT, registered_courses TEXT);');
+});
+
 
 
 //this code will be used for inserting into the departments table, returns a json with all subjects and their abbreviations
