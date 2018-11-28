1343f3c27d9b27900e0be936b9f7de32b974945c
Jacob Johnson <john6805@stthomas.edu>
2018-11-28 12:31:12 -0600

    create table if not exists

--- get-classes.js
+++ get-classes.js
@@ -22,10 +22,10 @@ var days = {
 //Create database
 database.serialize(() => {
     //Create Tables
-    database.run('CREATE TABLE departments(subject TEXT PRIMARY KEY, full_name TEXT);');
-    database.run('CREATE TABLE courses(subject TEXT, course_number TEXT, credits INT, name TEXT, description TEXT, PRIMARY KEY(subject, course_number));');
-    database.run('CREATE TABLE sections(crn INT PRIMARY KEY, subject TEXT, course_number TEXT, section_number TEXT, building TEXT, room TEXT, professors TEXT, times TEXT, capacity INT, registered TEXT);');
-    database.run('CREATE TABLE people(university_id INT PRIMARY KEY, position TEXT, password TEXT, first_name TEXT, last_name TEXT, registered_courses TEXT);');
+    database.run('CREATE TABLE IF NOT EXISTS departments(subject TEXT PRIMARY KEY, full_name TEXT);');
+    database.run('CREATE TABLE IF NOT EXISTS courses(subject TEXT, course_number TEXT, credits INT, name TEXT, description TEXT, PRIMARY KEY(subject, course_number));');
+    database.run('CREATE TABLE IF NOT EXISTS sections(crn INT PRIMARY KEY, subject TEXT, course_number TEXT, section_number TEXT, building TEXT, room TEXT, professors TEXT, times TEXT, capacity INT, registered TEXT);');
+    database.run('CREATE TABLE IF NOT EXISTS people(university_id INT PRIMARY KEY, position TEXT, password TEXT, first_name TEXT, last_name TEXT, registered_courses TEXT);');
 });
 
 
