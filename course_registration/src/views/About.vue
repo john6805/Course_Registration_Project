f99461447c204418613e1c62a819695848e1697c
Jacob Johnson <john6805@stthomas.edu>
2018-12-05 13:25:22 -0600

    used vue cli to create template for project

---
+++ course_registration/src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <header>About the Creators & Site</header>
+    <h1 style="clear: left;">John Mikkelson</h1>
+    <p>
+      <img id=johnpic src="../assets/johnpic.jpg" style="width:11%;height:100%;float:left;">
+      I am 22 years old born in Atlanta Georgia. I am currently a Senior at UST, and am double majoring in Computer Science and Finance. I currently work for Dispute Resolution Data, a company that aggregates and displays data regarding international arbitration and mediation cases. In my freetime, I am the President of the UST Rowing club and enjoy all the perks included. In the future I hope to work in New York for a few years in the Financial Technology sector.
+    </p>
+    <h1 style="clear: left;">Jacob Johnson</h1>
+    <p>
+      <img id=gtpic src="../assets/GT_rosterpic.jpg" style="float:left;">
+      GT Text goes here.
+    </p>
+    <h1 style="clear: left;">Ryan Jasperson</h1>
+    <p>
+      <img id=ryanpic src="../assets/familyphoto.jpg" style="width:15%; height:100%;float:left;">
+      Ryan Text goes here.
+    </p>
+    <h1 style="clear: left;">Tools used</h1>
+    <p>
+      Some tools we used:
+      <ul>
+        <li>Vue js framework</li>
+        <li>SQLite3 Database</li>
+        <li>Express</li>
+      </ul>
+    </p>
+  </div>
+</template>
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 const port = 8012;
-var public_dir = path.join(__dirname, 'client');
+var public_dir = path.join(__dirname, 'course_registration/dist');
 
 app.use(express.static(public_dir));
 
